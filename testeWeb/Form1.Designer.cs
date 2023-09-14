4c9e8803c3a5d7b91f8f753a6a289d942fff4c34
penhaMa <130068243+penhaMa@users.noreply.github.com>
2023-09-14 17:58:22 -0300

    Primeiro commit

---
+++ testeWeb/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace testeWeb
+{
+    partial class WebCam
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.salvar = new System.Windows.Forms.Button();
+            this.iniciar = new System.Windows.Forms.Button();
+            this.encerrar = new System.Windows.Forms.Button();
+            this.imagem = new System.Windows.Forms.PictureBox();
+            this.camera = new System.Windows.Forms.ComboBox();
+            this.recorte = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recorte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // salvar
+            // 
+            this.salvar.Location = new System.Drawing.Point(703, 413);
+            this.salvar.Name = "salvar";
+            this.salvar.Size = new System.Drawing.Size(219, 37);
+            this.salvar.TabIndex = 0;
+            this.salvar.Text = "Salvar";
+            this.salvar.UseVisualStyleBackColor = true;
+            this.salvar.Click += new System.EventHandler(this.salvar_Click);
+            // 
+            // iniciar
+            // 
+            this.iniciar.Location = new System.Drawing.Point(155, 449);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(219, 37);
+            this.iniciar.TabIndex = 1;
+            this.iniciar.Text = "Iniciar";
+            this.iniciar.UseVisualStyleBackColor = true;
+            this.iniciar.Click += new System.EventHandler(this.iniciar_Click);
+            // 
+            // encerrar
+            // 
+            this.encerrar.Location = new System.Drawing.Point(155, 504);
+            this.encerrar.Name = "encerrar";
+            this.encerrar.Size = new System.Drawing.Size(219, 37);
+            this.encerrar.TabIndex = 2;
+            this.encerrar.Text = "Encerrar";
+            this.encerrar.UseVisualStyleBackColor = true;
+            this.encerrar.Click += new System.EventHandler(this.encerrar_Click);
+            // 
+            // imagem
+            // 
+            this.imagem.Location = new System.Drawing.Point(32, 40);
+            this.imagem.Name = "imagem";
+            this.imagem.Size = new System.Drawing.Size(497, 312);
+            this.imagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagem.TabIndex = 3;
+            this.imagem.TabStop = false;
+            // 
+            // camera
+            // 
+            this.camera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.camera.FormattingEnabled = true;
+            this.camera.Location = new System.Drawing.Point(145, 413);
+            this.camera.Name = "camera";
+            this.camera.Size = new System.Drawing.Size(243, 24);
+            this.camera.TabIndex = 5;
+            // 
+            // recorte
+            // 
+            this.recorte.Location = new System.Drawing.Point(559, 40);
+            this.recorte.Name = "recorte";
+            this.recorte.Size = new System.Drawing.Size(497, 312);
+            this.recorte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.recorte.TabIndex = 6;
+            this.recorte.TabStop = false;
+            // 
+            // WebCam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 578);
+            this.Controls.Add(this.recorte);
+            this.Controls.Add(this.camera);
+            this.Controls.Add(this.imagem);
+            this.Controls.Add(this.encerrar);
+            this.Controls.Add(this.iniciar);
+            this.Controls.Add(this.salvar);
+            this.Name = "WebCam";
+            this.Text = "WebCam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WebCam_FormClosing);
+            this.Load += new System.EventHandler(this.WebCam_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.recorte)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button salvar;
+        private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.Button encerrar;
+        private System.Windows.Forms.PictureBox imagem;
+        private System.Windows.Forms.ComboBox camera;
+        private System.Windows.Forms.PictureBox recorte;
+    }
+}
+
---
+++ testeWeb/Form1.cs
@@ -0,0 +1,81 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+
+namespace testeWeb
+{
+    public partial class WebCam : Form
+    {
+        FilterInfoCollection filtro;
+        VideoCaptureDevice video;
+        public WebCam()
+        {
+            InitializeComponent();
+
+            filtro = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo filterInfo in filtro)
+                camera.Items.Add(filterInfo.Name);
+
+            camera.SelectedIndex = 0;
+            video = new VideoCaptureDevice();
+        }
+
+        private void WebCam_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iniciar_Click(object sender, EventArgs e)
+        {
+            video = new VideoCaptureDevice(filtro[camera.SelectedIndex].MonikerString);
+
+            video.NewFrame += video_Newframe;
+            video.Start();
+        }
+
+        private void video_Newframe(object sender, NewFrameEventArgs e)
+        {
+            imagem.Image = (Bitmap)e.Frame.Clone();
+        }
+
+        private void salvar_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetImage(imagem.Image);
+            recorte.Image = Clipboard.GetImage();
+            Clipboard.Clear();
+
+            recorte.Image.Save(@"C:\Users\penha\Desktop\imagem\imagem.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void encerrar_Click(object sender, EventArgs e)
+        {
+            Encerrar();
+        }
+
+        private void WebCam_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Encerrar();
+        }
+
+        private void Encerrar()
+        {
+            if (!(video == null))
+                if (video.IsRunning)
+                {
+                    video.NewFrame -= video_Newframe;
+                    video.SignalToStop();
+                    video = null;
+                }
+        }
+    }
+}
